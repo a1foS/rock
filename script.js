28866ceff5427ae237ce4ad27dc2501c973a72be
a1foS <barrossofia114@gmail.com>
2025-06-18 08:49:48 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+function enviar() {
+    alert("Cadastro feito com sucesso!")
+}
+
+//Variável para pegar o nome do visitante
+var usuario = prompt ("Digite o seu nome")
+
+//Pegar o valor da variável e imprimir na tela
+document.getElementById('visitante').innerHTML = usuario
